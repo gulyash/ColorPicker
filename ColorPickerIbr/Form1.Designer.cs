e33e1475d2a9c19d2ec89a233545abb045eb996c
Gulnara Ibragimova <igulnara6@gmail.com>
2017-10-08 21:14:07 +0500

    Добавьте файлы проекта.

---
+++ ColorPickerIbr/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace ColorPickerIbr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBarBlue = new System.Windows.Forms.TrackBar();
+            this.trackBarGreen = new System.Windows.Forms.TrackBar();
+            this.trackBarRed = new System.Windows.Forms.TrackBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.radioButtonBlack = new System.Windows.Forms.RadioButton();
+            this.radioButtonRed = new System.Windows.Forms.RadioButton();
+            this.radioButtonGreen = new System.Windows.Forms.RadioButton();
+            this.radioButtonYellow = new System.Windows.Forms.RadioButton();
+            this.radioButtonBlue = new System.Windows.Forms.RadioButton();
+            this.radioButtonMagenta = new System.Windows.Forms.RadioButton();
+            this.radioButtonCyan = new System.Windows.Forms.RadioButton();
+            this.radioButtonWhite = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarBlue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRed)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.trackBarBlue);
+            this.panel1.Controls.Add(this.trackBarGreen);
+            this.panel1.Controls.Add(this.trackBarRed);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(226, 153);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Синий";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Зеленый";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Красный";
+            // 
+            // trackBarBlue
+            // 
+            this.trackBarBlue.Location = new System.Drawing.Point(79, 105);
+            this.trackBarBlue.Maximum = 255;
+            this.trackBarBlue.Name = "trackBarBlue";
+            this.trackBarBlue.Size = new System.Drawing.Size(144, 45);
+            this.trackBarBlue.TabIndex = 2;
+            this.trackBarBlue.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBarBlue.Scroll += new System.EventHandler(this.trackBarBlue_Scroll);
+            // 
+            // trackBarGreen
+            // 
+            this.trackBarGreen.Location = new System.Drawing.Point(79, 54);
+            this.trackBarGreen.Maximum = 255;
+            this.trackBarGreen.Name = "trackBarGreen";
+            this.trackBarGreen.Size = new System.Drawing.Size(144, 45);
+            this.trackBarGreen.TabIndex = 1;
+            this.trackBarGreen.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBarGreen.Scroll += new System.EventHandler(this.trackBarGreen_Scroll);
+            // 
+            // trackBarRed
+            // 
+            this.trackBarRed.Location = new System.Drawing.Point(79, 3);
+            this.trackBarRed.Maximum = 255;
+            this.trackBarRed.Name = "trackBarRed";
+            this.trackBarRed.Size = new System.Drawing.Size(144, 45);
+            this.trackBarRed.TabIndex = 0;
+            this.trackBarRed.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBarRed.Scroll += new System.EventHandler(this.trackBarRed_Scroll);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(3, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(71, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Красный";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(3, 26);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(71, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Зеленый";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(3, 49);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(57, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Синий";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.checkBox1);
+            this.flowLayoutPanel1.Controls.Add(this.checkBox2);
+            this.flowLayoutPanel1.Controls.Add(this.checkBox3);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(244, 94);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(80, 71);
+            this.flowLayoutPanel1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(244, 171);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(80, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.Black;
+            this.pictureBox.Location = new System.Drawing.Point(244, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(80, 76);
+            this.pictureBox.TabIndex = 5;
+            this.pictureBox.TabStop = false;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.radioButtonBlack);
+            this.flowLayoutPanel2.Controls.Add(this.radioButtonRed);
+            this.flowLayoutPanel2.Controls.Add(this.radioButtonGreen);
+            this.flowLayoutPanel2.Controls.Add(this.radioButtonYellow);
+            this.flowLayoutPanel2.Controls.Add(this.radioButtonBlue);
+            this.flowLayoutPanel2.Controls.Add(this.radioButtonMagenta);
+            this.flowLayoutPanel2.Controls.Add(this.radioButtonCyan);
+            this.flowLayoutPanel2.Controls.Add(this.radioButtonWhite);
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(12, 171);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(226, 101);
+            this.flowLayoutPanel2.TabIndex = 6;
+            // 
+            // radioButtonBlack
+            // 
+            this.radioButtonBlack.AutoSize = true;
+            this.radioButtonBlack.Location = new System.Drawing.Point(3, 3);
+            this.radioButtonBlack.Name = "radioButtonBlack";
+            this.radioButtonBlack.Size = new System.Drawing.Size(65, 17);
+            this.radioButtonBlack.TabIndex = 5;
+            this.radioButtonBlack.TabStop = true;
+            this.radioButtonBlack.Text = "Черный";
+            this.radioButtonBlack.UseVisualStyleBackColor = true;
+            this.radioButtonBlack.CheckedChanged += new System.EventHandler(this.radioButtonBlack_CheckedChanged);
+            // 
+            // radioButtonRed
+            // 
+            this.radioButtonRed.AutoSize = true;
+            this.radioButtonRed.Location = new System.Drawing.Point(3, 26);
+            this.radioButtonRed.Name = "radioButtonRed";
+            this.radioButtonRed.Size = new System.Drawing.Size(70, 17);
+            this.radioButtonRed.TabIndex = 6;
+            this.radioButtonRed.TabStop = true;
+            this.radioButtonRed.Text = "Красный";
+            this.radioButtonRed.UseVisualStyleBackColor = true;
+            this.radioButtonRed.CheckedChanged += new System.EventHandler(this.radioButtonBlack_CheckedChanged);
+            // 
+            // radioButtonGreen
+            // 
+            this.radioButtonGreen.AutoSize = true;
+            this.radioButtonGreen.Location = new System.Drawing.Point(3, 49);
+            this.radioButtonGreen.Name = "radioButtonGreen";
+            this.radioButtonGreen.Size = new System.Drawing.Size(70, 17);
+            this.radioButtonGreen.TabIndex = 7;
+            this.radioButtonGreen.TabStop = true;
+            this.radioButtonGreen.Text = "Зеленый";
+            this.radioButtonGreen.UseVisualStyleBackColor = true;
+            this.radioButtonGreen.CheckedChanged += new System.EventHandler(this.radioButtonBlack_CheckedChanged);
+            // 
+            // radioButtonYellow
+            // 
+            this.radioButtonYellow.AutoSize = true;
+            this.radioButtonYellow.Location = new System.Drawing.Point(3, 72);
+            this.radioButtonYellow.Name = "radioButtonYellow";
+            this.radioButtonYellow.Size = new System.Drawing.Size(67, 17);
+            this.radioButtonYellow.TabIndex = 8;
+            this.radioButtonYellow.TabStop = true;
+            this.radioButtonYellow.Text = "Желтый";
+            this.radioButtonYellow.UseVisualStyleBackColor = true;
+            this.radioButtonYellow.CheckedChanged += new System.EventHandler(this.radioButtonBlack_CheckedChanged);
+            // 
+            // radioButtonBlue
+            // 
+            this.radioButtonBlue.AutoSize = true;
+            this.radioButtonBlue.Location = new System.Drawing.Point(79, 3);
+            this.radioButtonBlue.Name = "radioButtonBlue";
+            this.radioButtonBlue.Size = new System.Drawing.Size(56, 17);
+            this.radioButtonBlue.TabIndex = 9;
+            this.radioButtonBlue.TabStop = true;
+            this.radioButtonBlue.Text = "Синий";
+            this.radioButtonBlue.UseVisualStyleBackColor = true;
+            this.radioButtonBlue.CheckedChanged += new System.EventHandler(this.radioButtonBlack_CheckedChanged);
+            // 
+            // radioButtonMagenta
+            // 
+            this.radioButtonMagenta.AutoSize = true;
+            this.radioButtonMagenta.Location = new System.Drawing.Point(79, 26);
+            this.radioButtonMagenta.Name = "radioButtonMagenta";
+            this.radioButtonMagenta.Size = new System.Drawing.Size(91, 17);
+            this.radioButtonMagenta.TabIndex = 10;
+            this.radioButtonMagenta.TabStop = true;
+            this.radioButtonMagenta.Text = "Фиолетовый";
+            this.radioButtonMagenta.UseVisualStyleBackColor = true;
+            this.radioButtonMagenta.CheckedChanged += new System.EventHandler(this.radioButtonBlack_CheckedChanged);
+            // 
+            // radioButtonCyan
+            // 
+            this.radioButtonCyan.AutoSize = true;
+            this.radioButtonCyan.Location = new System.Drawing.Point(79, 49);
+            this.radioButtonCyan.Name = "radioButtonCyan";
+            this.radioButtonCyan.Size = new System.Drawing.Size(66, 17);
+            this.radioButtonCyan.TabIndex = 11;
+            this.radioButtonCyan.TabStop = true;
+            this.radioButtonCyan.Text = "Голубой";
+            this.radioButtonCyan.UseVisualStyleBackColor = true;
+            this.radioButtonCyan.CheckedChanged += new System.EventHandler(this.radioButtonBlack_CheckedChanged);
+            // 
+            // radioButtonWhite
+            // 
+            this.radioButtonWhite.AutoSize = true;
+            this.radioButtonWhite.Location = new System.Drawing.Point(79, 72);
+            this.radioButtonWhite.Name = "radioButtonWhite";
+            this.radioButtonWhite.Size = new System.Drawing.Size(58, 17);
+            this.radioButtonWhite.TabIndex = 12;
+            this.radioButtonWhite.TabStop = true;
+            this.radioButtonWhite.Text = "Белый";
+            this.radioButtonWhite.UseVisualStyleBackColor = true;
+            this.radioButtonWhite.CheckedChanged += new System.EventHandler(this.radioButtonBlack_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(337, 283);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarBlue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarRed)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TrackBar trackBarRed;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.RadioButton radioButtonBlack;
+        private System.Windows.Forms.RadioButton radioButtonRed;
+        private System.Windows.Forms.RadioButton radioButtonGreen;
+        private System.Windows.Forms.RadioButton radioButtonYellow;
+        private System.Windows.Forms.RadioButton radioButtonBlue;
+        private System.Windows.Forms.TrackBar trackBarBlue;
+        private System.Windows.Forms.TrackBar trackBarGreen;
+        private System.Windows.Forms.RadioButton radioButtonMagenta;
+        private System.Windows.Forms.RadioButton radioButtonCyan;
+        private System.Windows.Forms.RadioButton radioButtonWhite;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ColorPickerIbr/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorPickerIbr
+{
+    public partial class Form1 : Form
+    {
+        private readonly Dictionary<RadioButton, Color> commonColors;
+
+        public Color SelectedColor
+        {
+            set
+            {
+                pictureBox.BackColor = value;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            commonColors = new Dictionary<RadioButton, Color>
+            {
+                {radioButtonBlack, Color.Black },
+                {radioButtonRed, Color.Red },
+                {radioButtonGreen, Color.Green },
+                {radioButtonYellow, Color.Yellow },
+                {radioButtonBlue, Color.Blue },
+                {radioButtonMagenta, Color.DarkMagenta },
+                {radioButtonCyan, Color.Cyan },
+                {radioButtonWhite, Color.White },
+            };
+        }
+
+        private void trackBarRed_Scroll(object sender, EventArgs e)
+        {
+            SelectedColor = Color.FromArgb(trackBarRed.Value, trackBarGreen.Value, trackBarBlue.Value);
+        }
+
+        private void trackBarGreen_Scroll(object sender, EventArgs e)
+        {
+            SelectedColor = Color.FromArgb(trackBarRed.Value, trackBarGreen.Value, trackBarBlue.Value);
+        }
+
+        private void trackBarBlue_Scroll(object sender, EventArgs e)
+        {
+            SelectedColor = Color.FromArgb(trackBarRed.Value, trackBarGreen.Value, trackBarBlue.Value);
+        }
+
+        private void radioButtonBlack_CheckedChanged(object sender, EventArgs e)
+        {
+            var rb = (RadioButton)sender;
+            if (commonColors.ContainsKey(rb))
+            {
+                SelectedColor = commonColors[rb];
+            }
+        }
+    }
+}
